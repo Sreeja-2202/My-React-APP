11106d7e17e4a3be086f4f1a3b84680e50905d66
Sreeja <sreejagaddam511@gmail.com>
2025-08-04 20:20:42 +0530

    updated code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-// import Logo from "./images/Logo_fm.png"
+// import Logo from "./images/logo.png"
 /**
  * Header
  *  - Logo
@@ -9,6 +9,9 @@ import ReactDOM from "react-dom/client"
  *  - Search Component
  *  - ResturantCardContainer
  *    -- ResturantCard
+ *       -- Img
+ *       -- Name of Res,Star Rating ,Cuisine,delivery time,LOcation 
+ * 
  * Footer
  *  - CopyRights
  *  - Links
@@ -22,20 +25,22 @@ const Header =()=>{
         <div className="header">
 
             <div className="Logo-container"> 
-                <img className ="logo" src="" alt="Food Munch"/>
-                <img src="../"/>
+             
+                <img className="logo" src="https://ik.imagekit.io/q4iigq3f2/New%20Folder/logo.png?updatedAt=1754145966718" alt="Food Munch" />
+               
             </div>
 
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us </li>
-                    <li>Cart</li>
+                     <li><i className="fas fa-search"></i> Search</li>
+                    <li><i className="fas fa-home"></i> Home</li>
+                    <li><i className="fas fa-info-circle"></i> <span>About Us</span></li>
+                    <li><i className="fas fa-phone-alt"></i> <span>Contact Us</span> </li>
+                    <li><i className="fas fa-shopping-cart"></i> <span>Cart</span></li>
+                    <li><i className="fas fa-sign-in-alt"></i> <span>Sign in</span></li>
+
                     
                 </ul>
-                
-
             </div>
 
         </div>
@@ -43,10 +48,43 @@ const Header =()=>{
 
 }
 
+const ResturantCard =() =>{
+    return(
+        <div className="res-card">
+            <img  className ="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/mt2aggiscfl3yviatwng" alt="Bakery World"/>
+            <h3>Bakery World</h3>
+            <h4>Ice Creams,cakes,....</h4>
+            <h4>4.3 Star Rating</h4>
+            <h4>Vidya nagar</h4>
+
+
+
+
+
+        </div>
+    )
+}
+
+const Body =() =>{
+    return(
+        <div>
+            <div className="search">Search</div>
+            <div className="res-container">
+                <ResturantCard/>
+                <ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/>
+                <ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/><ResturantCard/>
+                
+
+            </div>
+        </div>
+    )
+}
+
 const AppLayout = ()=>{
     return (
         <div className="app">
           <Header/>
+          <Body/>
 
         </div>
     )
